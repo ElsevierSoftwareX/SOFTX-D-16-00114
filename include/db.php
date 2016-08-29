0782034d062dcfc886af59ab69a52d6d246bfb98
ElsevierSoftwareX <turyddu@gmail.com>
2016-08-29 11:45:56 +0200

    first commit

---
+++ include/db.php
@@ -0,0 +1,14 @@
+<?php
+// Host of the MySQL Server
+$db_host = 'localhost';
+// Username of the MySQL Server
+$db_user = 'root';
+// Password of the MySQL Server
+$db_password = '';
+// Name of Un-Code User Database
+$db_userdb = 'uncode.user';
+// Name of Un-Code Project Database
+$db_projectdb = 'uncode.projects';
+// Temp directory
+$tmp_directory = 'tmp/';
+?>
